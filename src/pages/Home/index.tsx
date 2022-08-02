645d63a46ff12cdaeccf28587d1e2851cd5ab7c3
cleisonmp <cleison.mp@gmail.com>
2022-08-02 19:26:14 -0300

    Extra home styling

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,13 +15,27 @@ export function Home() {
       <form action="">
         <TaskInfoContainer>
           <label htmlFor="task">Working on</label>
-          <TaskInput id="task" placeholder="Give your project a name" />
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Give your project a name"
+          />
+          <datalist id="task-suggestions">
+            <option value="Sugestão de nome de projeto 1"></option>
+            <option value="Sugestão de nome de projeto 2"></option>
+            <option value="Sugestão de nome de projeto 3"></option>
+            <option value="Sugestão de nome de projeto 4"></option>
+            <option value="Sugestão de nome de projeto 5"></option>
+          </datalist>
 
           <label htmlFor="minutesAmount">for</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutes.</span>
